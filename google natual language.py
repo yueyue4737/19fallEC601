8a75fe91af4cf135dcbab29c00e34193d4ef6109
szyszy315 <zeyusong@bu.edu>
2019-09-28 11:36:39 -0400

    Update google natual language.py

--- google natual language.py
+++ google natual language.py
@@ -3,12 +3,26 @@ from google.cloud.language_v1 import enums
 import six
 import gettweets as gt
 import json
+import time
+import nltk
+from nltk.corpus import stopwords
+def most_frequent(List):
+    unimportantwords =stopwords.words('english')
+    unimportantwords.extend(["It's","If"])
+    count = {}
+    for i in List:
+        if i in unimportantwords:
+            continue
+        if i not in count.keys():
+            count[i] = 1
+        else :
+            count[i] += 1
+    word = sorted(count.items(),key = lambda x:x[1] ,reverse = True)
+    return word[0:5]
 
 def sentiment(content):
 
     client = language_v1.LanguageServiceClient()
-
-    # content = 'Your text to analyze, e.g. Hello, world!'
 
     if isinstance(content, six.binary_type):
         content = content.decode('utf-8')
@@ -19,17 +33,30 @@ def sentiment(content):
     response = client.analyze_sentiment(document)
     sentiment = response.document_sentiment
     return(sentiment.score, sentiment.magnitude)
+
 if __name__ == '__main__':
+#get tweets
     tweets = []
     with open('tweettext.json') as file:
         tweets = json.load(file)
     score=0
     mag=0
     count = 0
+    freq = []
+#get score and magnitude of google natural language
     for i in range(len(tweets)) :
         score += sentiment(tweets[i][0])[0]
         mag += sentiment(tweets[i][0])[1]
         count += 1
+        tww = tweets[i][0].split(' ')
+        for t in tww:
+            if (len(t)) > 10:
+                continue
+            else:
+                freq.append(t)
+    fre=[]
+    fre.append(most_frequent(freq))
+    print('the most mentioned word is ',fre)
     score = score / count
     mag = mag / count
     if score > 0.5:
@@ -43,3 +70,4 @@ if __name__ == '__main__':
     elif score <=0.2:
         print(' Negative')
     else: print('Clearly Negative')
+    input('input anything to continue...')
